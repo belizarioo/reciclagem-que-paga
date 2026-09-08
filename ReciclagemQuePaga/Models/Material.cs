0731d27fc277970e998c696c9283ac7ce05b5c9f
henrique.bsilva25 <henrique.bsilva25@VPR0681557W11-1>
2026-09-08 15:36:42 -0300

    feature: adiciona modelos e configuração do DbContext

---
+++ ReciclagemQuePaga/Models/Material.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReciclagemQuePaga.Models
+{
+    public class Material
+    {
+        public int MaterialId { get; set; }
+        public string TipoMaterial { get; set; } = String.Empty;
+        public decimal PrecoMaterialPorKilo { get; set; }
+    }
+}
--- ReciclagemQuePaga/Models/Usuario.cs
+++ ReciclagemQuePaga/Models/Usuario.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Microsoft.Identity.Client;
+using System;
 
-public class Class1
+public class Usuario
 {
-	public Class1()
-	{
-	}
+	public int UsuarioId { get; set; }
+	public string NomeUsuario { get; set; } = String.Empty;
+	public string EmailUsuario { get; set; } = String.Empty;
+    public string SenhaUsuario { get; set; } = String.Empty;
+    public string CpfUsuario { get; set; } = String.Empty;
+
 }
