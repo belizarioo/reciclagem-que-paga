c9f33f12a0408af0b70389ca3506da22b2555460
henrique.bsilva25 <henrique.bsilva25@VPR0681557W11-1>
2026-09-10 16:19:04 -0300

    feat: adiciona repositories e services para usuario, material e transação

---
+++ ReciclagemQuePaga/Data/UsuarioRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReciclagemQuePaga.Data
+{
+    public class UsuarioRepository
+    {
+        private readonly DataBaseConnection _context;
+        public UsuarioRepository(DataBaseConnection context)
+        {
+            _context = context;
+        }
+        public void Cadastrar(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+        }
+
+        public Usuario? BuscarPorId(int id)
+        {
+            return _context.Usuarios.Find(id);
+        }
+    }
+}
